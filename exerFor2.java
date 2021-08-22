c977738220cd9e70d3ffefa3000b34beb90f0fa6
Samuel-prog182 <samuellb.o.w.182@hotmail.com>
2021-08-22 19:32:34 +0000

    exercicio 02

---
+++ exerFor2.java
@@ -0,0 +1,14 @@
+public class exerFor2{
+    public static void main(String[] args){
+        System.out.println("Exercicio 02 - Imprima a soma de 1 a 1000");
+
+       //Declaração de variaveis
+        int soma = 0;
+        int j;
+
+         //Inicio do For
+        for( j = 1; j <= 1000; j++){soma = soma + j;
+        } //Fim do For
+        System.out.println("A soma é: " + soma);  //Impressão do resultado
+   }
+}
